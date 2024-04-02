35e46a60a3bcbfb11f52b1aa06fd8ddded84d3a8
TheElenwe <elenwe97@hotmail.com>
2024-04-02 15:09:08 +0300

    json

--- frontend/src/components/EventsNavigation.js
+++ frontend/src/components/EventsNavigation.js
@@ -1,29 +1,31 @@
-import { NavLink } from "react-router-dom";
-import classes from "./MainNavigation.module.css";
+import { NavLink } from 'react-router-dom';
 
-function MainNavigation() {
+import classes from './EventsNavigation.module.css';
+
+function EventsNavigation() {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
             <NavLink
-              to="/"
+              to="/events"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
+              end
             >
-              Home
+              All Events
             </NavLink>
           </li>
           <li>
             <NavLink
-              to="/products"
+              to="/events/new"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
             >
-              Products
+              New Event
             </NavLink>
           </li>
         </ul>
@@ -32,4 +34,4 @@ function MainNavigation() {
   );
 }
 
-export default MainNavigation;
+export default EventsNavigation;
